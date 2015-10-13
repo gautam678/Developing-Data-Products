04e61b4618d5b3efe402439072d92e010af23230
gautam678 <gautam678@gmail.com>
2015-10-13 12:05:14 +0530

    Updates ui file with Documentation

--- ui.R
+++ ui.R
@@ -11,8 +11,12 @@ shinyUI(pageWithSidebar(
                 label = "Choose Y",
                 choices = c('Girth', 'Height', 'Volume'),
                 selected = "Volume")
+
+    
   ),
   mainPanel(
+    h1('Graph to compare relation between different parameters'),
+    p('The dataset taken in this case is the trees dataset which has the following parameters : Height, Volume and Girth. I have plotted a graph between any of the parameters. The graph is user controlled where the user can switch between any of the three parameters.'),
     showOutput("myChart", "polycharts")
   )
 ))
